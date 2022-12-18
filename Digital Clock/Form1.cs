7e8e58ad1090b542d42e27bd0247a245cfbe4ca6
Abdullah Ciftci <abdullahciftci1515@gmail.com>
2022-12-18 16:59:05 +0100

    Projektdateien hinzufügen.

---
+++ Digital Clock/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+ 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Start();
+            
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("T");
+        }
+    }
+}
